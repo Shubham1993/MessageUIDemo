d40d863cc5bc4a42f3eab2002e2e5f795ff76f30
Shubham <shubham@shubhams-mac.local>
2017-05-01 00:28:10 +0530

    send message and email

--- EmailSender/ViewController.m
+++ EmailSender/ViewController.m
@@ -13,7 +13,10 @@
 @end
 
 @implementation ViewController
-
+{
+    MFMailComposeViewController* mailComposeViewController;
+    MFMessageComposeViewController *messageComposeViewController;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
@@ -22,6 +25,44 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)sendEMail:(id)sender {
+    
+    if (![MFMailComposeViewController canSendMail]) {
+        NSLog(@"Can not send Email");
+    }else{
+        
+        mailComposeViewController = [[MFMailComposeViewController alloc] init];
+        mailComposeViewController.mailComposeDelegate = self;
+        [self presentViewController:mailComposeViewController animated:YES completion:nil];
+    }
+    
+}
+- (IBAction)sendMessage:(id)sender {
+    
+    if (![MFMessageComposeViewController canSendText]) {
+        NSLog(@"Can not send message");
+    }else{
+        messageComposeViewController = [[MFMessageComposeViewController alloc] init];
+        messageComposeViewController.messageComposeDelegate = self;
+        [self presentViewController:messageComposeViewController animated:YES completion:nil];
+    }
+}
+
+
+-(void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error{
+    
+    if (error) {
+        NSLog(@"failed to send mail");
+    }
+    [mailComposeViewController dismissViewControllerAnimated:YES completion:nil];
+}
+
+-(void)messageComposeViewController:(MFMessageComposeViewController *)controller didFinishWithResult:(MessageComposeResult)result{
+    
+    [messageComposeViewController dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
